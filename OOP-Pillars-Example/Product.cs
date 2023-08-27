b589e0e9d347690e0980108ede3e2780529bd1ce
MUBTE <MUBTE@DESKTOP-PPFVO10>
2023-08-27 13:00:52 -0700

    Add project files.

---
+++ OOP-Pillars-Example/Product.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Pillars_Example
+{
+    //Encapsulation
+    class Product
+    {
+        private string name;
+        private double price;
+
+        public Product(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public double CalculateDiscount(double discountPercentage)
+        {
+            return price * (1 - discountPercentage / 100);
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+    }
+
+
+    //Abstraction
+    abstract class PaymentMethod
+    {
+        public abstract void ProcessPayment(double amount);
+    }
+
+    class CreditCardPayment : PaymentMethod
+    {
+        private string cardNumber;
+
+        public CreditCardPayment(string cardNumber)
+        {
+            this.cardNumber = cardNumber;
+        }
+
+        public override void ProcessPayment(double amount)
+        {
+            // Process payment using the credit card
+        }
+    }
+
+    //Inheritance
+    class ShoppingCartItem
+    {
+        protected Product product;
+        protected int quantity;
+        public ShoppingCartItem(Product product, int quantity)
+        {
+            this.product = product;
+            this.quantity = quantity;
+        }
+        public virtual double CalculateTotalPrice()
+        {
+            return product.CalculateDiscount(0) * quantity;
+        }
+    }
+
+    class DiscountedShoppingCartItem : ShoppingCartItem
+    {
+        private double discountPercentage;
+        public DiscountedShoppingCartItem(Product product, int quantity, double discountPercentage)
+            : base(product, quantity)
+        {
+            this.discountPercentage = discountPercentage;
+        }
+        public override double CalculateTotalPrice()
+        {
+            return product.CalculateDiscount(discountPercentage) * quantity;
+        }
+    }
+
+
+    //Polymorphism
+    class Order
+    {
+        private List<ShoppingCartItem> items;
+
+        public Order()
+        {
+            items = new List<ShoppingCartItem>();
+        }
+
+        public void AddItem(ShoppingCartItem item)
+        {
+            items.Add(item);
+        }
+
+        public double CalculateTotalOrderPrice()
+        {
+            double total = 0;
+            foreach (var item in items)
+            {
+                total += item.CalculateTotalPrice();
+            }
+            return total;
+        }
+    }
+
+
+}
---
+++ OOP-Pillars-Example/Program.cs
@@ -0,0 +1,14 @@
+﻿using OOP_Pillars_Example;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Product product = new Product("Web 3.0 By Zeeshan Usmani", 1500);
+        ShoppingCartItem shoppingCartItem = new ShoppingCartItem(product, 2);
+        Order obj = new Order();
+        obj.AddItem(shoppingCartItem);
+        Console.WriteLine("Calculate Total OrderPrice ,{0}", obj.CalculateTotalOrderPrice());
+        Console.ReadLine();
+    }
+}
